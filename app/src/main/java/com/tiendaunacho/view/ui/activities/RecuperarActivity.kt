f9ebde57c134979618bbc83941883b9fad4b75d0
macastilloq <miguela.castilloq@gmail.com>
2022-11-04 11:42:50 -0500

    Sprint 2

---
+++ app/src/main/java/com/tiendaunacho/view/ui/activities/RecuperarActivity.kt
@@ -0,0 +1,23 @@
+package com.tiendaunacho.view.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.tiendaunacho.R
+
+class RecuperarActivity : AppCompatActivity() {
+
+    lateinit var Bregresa: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*setContentView(R.layout.activity_recuperar)
+        Bregresa=findViewById(R.id.Bregresa)
+
+        Bregresa.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java ))
+        }
+
+         */
+    }
+}
