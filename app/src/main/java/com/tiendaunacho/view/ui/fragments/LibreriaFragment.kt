3e191269b512388bd085442e8acc5a0110ac7b05
macastilloq <miguela.castilloq@gmail.com>
2022-11-08 12:03:18 -0500

    Sprint 3 2022-11-08

--- app/src/main/java/com/tiendaunacho/view/ui/fragments/LibreriaFragment.kt
+++ app/src/main/java/com/tiendaunacho/view/ui/fragments/LibreriaFragment.kt
@@ -5,18 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tiendaunacho.R
+import com.tiendaunacho.view.adapter.LibreryAdapter
 
 
 class LibreriaFragment : Fragment() {
-
+    lateinit var recycleLib:RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_libreria, container, false)
+        val view= inflater.inflate(R.layout.fragment_libreria, container, false)
+        recycleLib=view.findViewById(R.id.recyclerview)
+        val adapter=LibreryAdapter()
+        recycleLib.layoutManager=LinearLayoutManager(context)
+        recycleLib.adapter=adapter
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btm=view.findViewById<BottomNavigationView>(R.id.buttonnvigation)
+        btm.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.home -> findNavController().navigate(R.id.action_libreriaFragment_to_homeFragment)
+                R.id.perfilpersona -> findNavController().navigate(R.id.action_libreriaFragment_to_perfilFragment)
+                R.id.map -> findNavController().navigate(R.id.action_libreriaFragment_to_mapaFragment)
+            }
+
+        }
+    }
 
 }
